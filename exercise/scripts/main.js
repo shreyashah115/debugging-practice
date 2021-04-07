bc28886263b5b976b7964edc4d832aef93f86faa
Shreya <shreyashah115@gmail.com>
2021-04-07 16:13:35 -0400

    Change card length condition

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -15,7 +15,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
       // If we haven't selected 2 cards yet, add the current card to the
       // collection of selected cards and apply the correct CSS class.
-      if (selectedCards.length < 2) {
+      if (selectedCards.length < 3) {
         card.classList.add("is-selected");
       }
 
